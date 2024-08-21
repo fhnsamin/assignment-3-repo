3747e7a46f52c2a41e715ad401afda3bf86bbb28
fhnsamin <fhnislam@gmail.com>
2024-08-22 02:58:18 +0600

    Made about us section

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-1": "#1c1c1c",
+        "dark-2": "#212121",
+        "dark-3": "#494949",
+        primary: "#ABEF5F",
+      },
+    },
+  },
+  plugins: [],
+};
